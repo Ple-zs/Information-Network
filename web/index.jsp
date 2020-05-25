de6716a8c8fc185cb64a9156e61a0d26ebe0eef6
Ple-zs <2506381226@qq.com>
2020-05-25 12:26:05 +0800

    Initial commit

---
+++ web/index.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 86135
+  Date: 2020/5/25
+  Time: 11:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>资讯网首页</title>
+    <link rel="stylesheet" type="text/css" href="css/head-Dynamic_Bulletin.css" />
+  </head>
+  <body>
+  <div class='wrapper'>
+    <div class='flip'></div>
+  </div>
+  <input class="header-panel-input" placeholder='CONTENT' type='text'>
+  <script src='js/jquery-3.4.1.js'></script>
+  <script src="js/head-Dynamic_Bulletin.js"></script>
+  </body>
+</html>
---
+++ web/js/head-Dynamic_Bulletin.js
@@ -0,0 +1,75 @@
+var charList = [" ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "\"", "$", "%", "&", "/", "(", ")", "=", "?", "+", "*", "#", "'", ":", ";", "-", ".", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var randomAirportNames = ["EDINBURGH","DYCE","GLASGOW","LONDON HEATHROW","JFK INTERNATIONAL","FRANKFURT","BERLIN","PARIS","ROME","WASHINGTON DC","HONG KONG","SYDNEY","DUBLIN","MOON","SKIPHOL","WARSAW","HONOLULU","ZURICH"];
+
+$(document).ready(function(){
+  anewNested();
+  $(".header-panel-input").focus();
+    var msg = "HELLO-WORLD-TWO";
+  $(".header-panel-input").val(msg);
+  showContent(msg);
+  $(".header-panel-input").blur(function(){
+	//赋值显示
+	var msg = $("input").val();
+	showContent(msg);
+  });
+});
+
+function switchChar(char,n){
+  var current = $(".flip").eq(n).attr("value");
+  var start = 0;
+  for(var i=0;i<charList.length;i++){
+    if(charList[i] == current){
+      start = i;
+      break;
+    } 
+  }
+  var complete = false;
+  for(var i=start;i<charList.length;i++){
+    if(charList[i] == char){
+      complete = true;
+      break;
+    }
+    $(".flip").eq(n).attr("value",char);
+    $(".flip").eq(n).find("ul").animate({top: "-=70px"},100);
+  }
+  if(complete == false){
+    $(".flip").eq(n).find("ul").animate({top: "0px"},100);
+    start = 0;
+     for(var i=start;i<charList.length;i++){
+      if(charList[i] == char){
+        break;
+        complete = true;
+      }
+      $(".flip").eq(n).attr("value",char);
+      $(".flip").eq(n).find("ul").animate({top: "-=70px"},100);
+    }
+  }
+}
+
+function anewNested(){
+	$(".flip").each(function(index){
+    $(this).append("<ul></ul>");
+    $(this).attr("value"," ");
+		for(var i=0;i<charList.length;i++){
+		  var char = charList[i];
+		  $("ul", this).append("<li>"+char+"</li>");
+		}
+	});
+}
+
+function showContent(msg) {
+    //随机显示
+    //var random = Math.round(Math.random()*randomAirportNames.length);
+    //var msg = randomAirportNames[random]+"                                    ";
+    $(".wrapper").children().remove();
+    for(var k =0;k<msg.length;k++){
+        $(".wrapper").append("<div class='flip'></div>");
+    }
+    anewNested();
+    var msgEx = msg.split("");
+    var showGridCount = msgEx.length;
+    for(var k=0;k<msgEx.length;k++){
+        switchChar(msgEx[k],k);
+    }
+}
